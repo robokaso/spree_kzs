b707639b3010e374e49f915b6f295c17d437014d
Robert Kasanický <robokaso@gmail.com>
2013-05-19 20:29:42 +0200

    slider ordering

--- app/controllers/spree/home_controller_decorator.rb
+++ app/controllers/spree/home_controller_decorator.rb
@@ -2,13 +2,13 @@ Spree::HomeController.class_eval do
 
   def index
     slider = Spree::Taxon.where(:name => 'Slider').first
-    @slider_products = slider.products.active if slider
+    @slider_products = slider.products.active.reorder('available_on') if slider
 
-    featured = Spree::Taxon.where(:name => 'Featured').first
-    @featured_products = featured.products.active if featured
+    #featured = Spree::Taxon.where(:name => 'Featured').first
+    #@featured_products = featured.products.active if featured
 
-    latest = Spree::Taxon.where(:name => 'Latest').first
-    @latest_products = latest.products.active if latest
+    #latest = Spree::Taxon.where(:name => 'Latest').first
+    #@latest_products = latest.products.active if latest
   end
 
 end
